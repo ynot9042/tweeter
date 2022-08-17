72849dc095c377f7cc02a0c1c1a7bfc923a73534
Jeffrey Ho <jeff.ho@hotmail.com>
2022-08-17 04:11:33 +0000

    used jquery and ajax for post route

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,38 +6,35 @@
 
 const data = [
   {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac",
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
+    content: {
+      text: "If I have seen further it is by standing on the shoulders of giants",
     },
-    "created_at": 1461116232227
+    created_at: 1461116232227,
   },
   {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
+    user: {
+      name: "Descartes",
+      avatars: "https://i.imgur.com/nlhLi3I.png",
+      handle: "@rd",
     },
-    "created_at": 1461113959088
-  }
-]
+    content: {
+      text: "Je pense , donc je suis",
+    },
+    created_at: 1461113959088,
+  },
+];
 
-
-
-$(document).ready(function () { 
-
-  const renderTweets = function(tweets) {
+$(document).ready(function () {
+  const renderTweets = function (tweets) {
     for (const tweet of tweets) {
       const $tweet = $(createTweetElement(tweet));
       $(".tweets-container").append($tweet);
-    };
+    }
   };
 
   const createTweetElement = function (tweetData) {
@@ -65,15 +62,23 @@ $(document).ready(function () {
       </div>
     </footer>
   </article>`;
-  
-    return tweetObj;  
+
+    return tweetObj;
   };
-  
 
   renderTweets(data);
 
+  $(".tweet-form").submit(function (event) {
+    event.preventDefault();
+    console.log($(this));
+    $.ajax({
+      type: "POST",
+      url: "/tweets",
+      data: $(this).serialize(),
+    });
+  });
+
+  const loadTweets = function() {
+
+  };
 });
-
-
-
-
